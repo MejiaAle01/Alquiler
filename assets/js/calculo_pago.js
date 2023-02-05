75008553d905e6e13a5be069591ffe81ce0adccc
Alejandro Mejía <83927564+MejiaAle01@users.noreply.github.com>
2023-02-05 12:29:17 -0600

    Update calculo_pago.js

--- assets/js/calculo_pago.js
+++ assets/js/calculo_pago.js
@@ -1,8 +1,8 @@
 function Precio() {
-    var totalpago = 0.00;
+    let totalpago = 0.00;
 
-    var cantCarros = document.getElementById('cantcarros').value;
-    var precio = document.getElementById('price').value;
+    let cantCarros = document.getElementById('cantcarros').value;
+    let precio = document.getElementById('price').value;
 
     totalpago = parseFloat(cantCarros * precio).toFixed(2);
 
